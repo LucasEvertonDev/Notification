8696ef37c4bd320ecafbc1610c9cd29eecd5001a
Lcs Everton <lcseverton@outlook.com>
2023-11-04 13:07:56 -0300

    Incluir classe de resultado tipado

---
+++ Notification/Notifications/IResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Notification.Notifications;
+
+/// <summary>
+/// Interface que deve ser implementada por todos os retornos do result.
+/// </summary>
+public interface IContentResult
+{
+}
