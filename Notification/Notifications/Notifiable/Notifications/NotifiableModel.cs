f7ac6507f969fd02c906afa60b0b9e3fa6c71f22
Lcs Everton <lcseverton@outlook.com>
2023-11-11 11:46:12 -0300

    Atualização da versão

--- Notification/Notifications/Notifiable/Notifications/NotifiableModel.cs
+++ Notification/Notifications/Notifiable/Notifications/NotifiableModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Notification.Notifications;
 using Notification.Notifications.Context;
 using Notification.Notifications.Notifiable.Notifications.Base;
@@ -8,9 +9,11 @@ namespace Notifications.Notifiable.Notifications;
 public partial class Notifiable<TEntity> : INotifiableModel
 {
     [JsonIgnore]
+    [NotMapped]
     public Result Result { get; set; } = new Result(new NotificationContext());
 
     [JsonIgnore]
+    [NotMapped]
     private static EntityInfo EntityInfo => new ()
     {
         Name = typeof(TEntity).Name,
